dfd2eb55120dd11796517552870e59e05a4261e2
Victor D. Sandiego <codepuppy@balticsoap.com>
2019-01-25 15:12:34 -0600

    Create custom window for the application

--- Source/DrumMaster/View/Window/MainWindow.xaml.cs
+++ Source/DrumMaster/View/Window/MainWindow.xaml.cs
@@ -1,11 +1,12 @@
-﻿using System.Windows;
+﻿using Restless.App.DrumMaster.Controls;
+using System.Windows;
 
 namespace Restless.App.DrumMaster.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : ApplicationWindow
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="MainWindow"/> class.
