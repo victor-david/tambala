a44d3fcfae34ea25c3a9c12c0e35831df7bcecb1
Victor D. Sandiego <codepuppy@balticsoap.com>
2019-02-05 20:44:06 -0600

    Update simple local installer

--- install.cmd
+++ install.cmd
@@ -3,7 +3,7 @@ rem Provides a simple means of installing the app.
 rem You just need to get the executable and .dlls in one directory.
 setlocal
 set ROOT=%~dp0
-set BIN=%ROOT%Source\Tambala\bin\x64\release
+set BIN=%ROOT%src\Tambala\bin\x64\release
 rem Change the destination path as needed and use this file to install.
 set DEST=D:\Utility\Tambala
 if exist "%BIN%\Tambala.exe" (
