7d25e36b30d148034c9020586c146e70e6476254
Victor D. Sandiego <codepuppy@balticsoap.com>
2019-02-06 20:30:53 -0600

    Update license

--- src/Tambala/ViewModel/ProjectContainerViewModel.cs
+++ src/Tambala/ViewModel/ProjectContainerViewModel.cs
@@ -1,7 +1,7 @@
 /*
  * Copyright 2019 Victor D. Sandiego
  * This file is part of Tambala.
- * Tambala is free software: you can redistribute it and/or modify it under the terms of the MIT License
+ * Tambala is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License v3.0
  * Tambala is distributed in the hope that it will be useful, but without warranty of any kind.
 */
 using Microsoft.Win32;
