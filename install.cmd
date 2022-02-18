686ef54f1a3d7fc4579cba8c605f2b5a27cd809a
Victor D. Sandiego <codepuppy@balticsoap.com>
2022-02-18 17:01:23 -0600

    Update simple installer
    Simple installer copies files into a directory. Don't need anything fancy.

--- install.cmd
+++ install.cmd
@@ -3,17 +3,14 @@ rem Provides a simple means of installing the app.
 rem You just need to get the executable and .dlls in one directory.
 setlocal
 set ROOT=%~dp0
-set BIN=%ROOT%src\Tambala\bin\x64\release
+set BIN=%ROOT%src\Tambala\bin\Release\net5.0-windows
+
 rem Change the destination path as needed and use this file to install.
-set DEST=D:\Utility\Tambala
+set DEST=D:\Utility\Tambala5.0
 if exist "%BIN%\Tambala.exe" (
   xcopy "%BIN%\Tambala.exe" %DEST% /Y
+  xcopy "%BIN%\*.json" %DEST% /Y
   xcopy "%BIN%\*.dll" %DEST% /E /Y
-)
-
-rem Copy CHM file if it's there
-if exist "%ROOT%\Help\Tambala.Reference.chm" (
-  xcopy "%ROOT%\Help\Tambala.Reference.chm" %DEST% /Y
 )
 
 dir %DEST%
