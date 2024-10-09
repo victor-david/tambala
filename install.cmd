58453ad8d4f88300fbfabd381dcf6ec6d8e1d39b
Victor D. Sandiego <codepuppy@balticsoap.com>
2024-10-09 13:30:14 -0600

    Update target framework

--- install.cmd
+++ install.cmd
@@ -3,7 +3,7 @@ rem Provides a simple means of installing the app.
 rem You just need to get the executable and .dlls in one directory.
 setlocal
 set ROOT=%~dp0
-set BIN=%ROOT%src\Tambala\bin\Release\net5.0-windows
+set BIN=%ROOT%src\Tambala\bin\Release\net7.0-windows
 
 rem Change the destination path as needed and use this file to install.
 set DEST=D:\Utility\Tambala5.0
